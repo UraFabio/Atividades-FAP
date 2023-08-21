4f4012bd03f383e6856def2943ce9a69f2767155
UraFabio <fabioura10@gmail.com>
2023-08-21 15:32:38 -0300

    atividade 3

---
+++ Plataforma Softex/JavaScript/Módulo 1/Atividade 3.js
@@ -0,0 +1,26 @@
+var booleano 
+var bigInt
+var string
+var symbol
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
+
+booleano = bigInt = string = symbol = null
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
+
+booleano = false
+bigInt = 5633365
+string = "Olá mundo"
+symbol = Symbol()
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
