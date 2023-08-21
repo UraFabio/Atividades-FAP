dc33be0b445754cdac5ba6100760a3d33a6b01cf
UraFabio <fabioura10@gmail.com>
2023-08-21 15:53:14 -0300

    atividade 5

---
+++ Plataforma Softex/JavaScript/Módulo 1/Atividade 5.js
@@ -0,0 +1,10 @@
+/*Deve ser executado no programiz*/
+
+console.log("Digite as 3 notas entre 1 e 10: ")
+
+let nota1 = Number(prompt(">>"))
+let nota2 = Number(prompt(">>"))
+let nota3 = Number(prompt(">>"))
+let media = (nota1+nota2+nota3)/3
+
+console.log(`A média das notas é de ${media}`)
