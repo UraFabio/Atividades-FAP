2d6c725b635930cc60127c29f224f83cd8783af3
UraFabio <fabioura10@gmail.com>
2023-08-21 15:44:37 -0300

    atividade 4

---
+++ Plataforma Softex/JavaScript/Módulo 1/Atividade 4.js
@@ -0,0 +1,11 @@
+/*Deve ser executado no programiz*/
+
+let nome = prompt("Digite seu nome: ")
+let salario = Number(prompt("Digite seu salário: "))
+let idade = Number(prompt("Digite sua idade: "))
+let diploma = prompt("Possui diploma? 1 para sim e 0 para não: ") == 1? true: false;
+
+console.log(nome)
+console.log(salario)
+console.log(idade)
+console.log(diploma)
